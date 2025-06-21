3c4fd5ef8787c35880d727c3b64fbfbc179b7311
KohiiTM <jamesandallen.dev@gmail.com>
2025-06-21 12:14:19 -0700

    Refactor: Main.java -> WindowManager.java

--- src/main/java/game/engine/Main.java
+++ src/main/java/game/engine/Main.java
@@ -1,92 +1,40 @@
 package game.engine;
 
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
+import game.engine.core.WindowManager;
 
-import java.nio.IntBuffer;
-
-import static org.lwjgl.glfw.Callbacks.*;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL.createCapabilities;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
-
 
 public class Main {
 
-    private long window;
+    private WindowManager windowManager;
 
     public void run() {
         init();
         loop();
 
-        // Free window callbacks and destroy window
-        glfwFreeCallbacks(window);
-        glfwDestroyWindow(window);
-
-        glfwTerminate();
-        glfwSetErrorCallback(null).free();
+        cleanup();
     }
 
     private void init() {
-        GLFWErrorCallback.createPrint(System.err).set();
-
-        if (!glfwInit())
-            throw new IllegalStateException("Unable to initialize GLFW");
-
-        glfwDefaultWindowHints();
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-
-        // Create window
-        window = glfwCreateWindow(800, 600, "Hello LWJGL + OpenGL!", NULL, NULL);
-        if (window == NULL)
-            throw new RuntimeException("Failed to create the GLFW window");
-
-        // Key callback
-        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-           if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-               glfwSetWindowShouldClose(window, true);
-        });
-
-        // Get thread stack -> push new frame
-        try (MemoryStack stack = stackPush()) {
-            IntBuffer pWidth = stack.mallocInt(1);
-            IntBuffer pHeight = stack.mallocInt(1);
-
-            glfwGetWindowSize(window, pWidth, pHeight);
-
-            // Primary monitor resolution
-            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-           glfwSetWindowPos(
-                   window,
-                   (vidmode.width() - pWidth.get(0)) / 2,
-                   (vidmode.height() - pHeight.get(0)) / 2
-           ) ;
-        }
-
-        glfwMakeContextCurrent(window);
-        // v-sync
-        glfwSwapInterval(1);
-
-        glfwShowWindow(window);
+        windowManager = new WindowManager("My LWJGL + OpenGL Game", 800, 600, true);
+        windowManager.init();
     }
+
     private void loop() {
-        createCapabilities();
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-        while (!glfwWindowShouldClose(window)) {
+        while (!windowManager.windowShouldClose()) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            glfwSwapBuffers(window);
-            glfwPollEvents();
+
+            windowManager.update();
         }
+    }
+
+    private void cleanup() {
+        windowManager.cleanup();
     }
 
     public static void main(String[] args) {
         new Main().run();
     }
-
 }
--- src/main/java/game/engine/core/WindowManager.java
+++ src/main/java/game/engine/core/WindowManager.java
@@ -1,4 +1,152 @@
 package game.engine.core;
 
+import org.joml.Matrix4f;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+
 public class WindowManager {
+
+    public static final float FOV = (float) Math.toRadians(60);
+    public static final float Z_NEAR = 0.1f;
+    public static final float Z_FAR = 1000.0f;
+
+    private final String title;
+    private long windowHandle;
+    private int width, height;
+    private boolean resize;
+    private boolean vSync;
+
+    private final Matrix4f projectionMatrix;
+
+    public WindowManager(String title, int width, int height, boolean vSync) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+        this.vSync = vSync;
+        this.resize = false;
+        projectionMatrix = new Matrix4f();
+    }
+
+    public void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+
+        windowHandle = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (windowHandle == NULL)
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        glfwSetKeyCallback(windowHandle, (window, key, scancode, action, mods) -> {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                glfwSetWindowShouldClose(window, true);
+        });
+
+        glfwSetFramebufferSizeCallback(windowHandle, (window, width, height) -> {
+            this.width = width;
+            this.height = height;
+            this.resize = true;
+        });
+
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+
+            glfwGetWindowSize(windowHandle, pWidth, pHeight);
+
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            glfwSetWindowPos(
+                    windowHandle,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        }
+
+        glfwMakeContextCurrent(windowHandle);
+
+        if (vSync) {
+            glfwSwapInterval(1);
+        } else {
+            glfwSwapInterval(0);
+        }
+
+        glfwShowWindow(windowHandle);
+
+        GL.createCapabilities();
+
+        updateProjectionMatrix();
+    }
+
+    public void updateProjectionMatrix() {
+        float aspectRatio = (float) width / height;
+        projectionMatrix.setPerspective(FOV, aspectRatio, Z_NEAR, Z_FAR);
+    }
+
+    public boolean windowShouldClose() {
+        return glfwWindowShouldClose(windowHandle);
+    }
+
+    public void update() {
+        glfwSwapBuffers(windowHandle);
+        glfwPollEvents();
+
+        if (resize) {
+            glViewport(0, 0, width, height);
+            updateProjectionMatrix();
+            resize = false;
+        }
+    }
+
+    public void cleanup() {
+        glfwFreeCallbacks(windowHandle);
+        glfwDestroyWindow(windowHandle);
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    public long getWindowHandle() {
+        return windowHandle;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isvSync() {
+        return vSync;
+    }
+
+    public Matrix4f getProjectionMatrix() {
+        return projectionMatrix;
+    }
 }
